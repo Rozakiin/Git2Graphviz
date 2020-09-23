610e5f2bf5d073a2747f93202ff3875705243782
Ruari <ruari.armstrong@gmail.com>
2020-09-23 15:00:59 +0100

    update install.sh to run on linux

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,9 +9,11 @@ if [ $? -ne 0 ]; then
     echo "Failed to install Boost." 1>&2
     exit 1
 fi
-./b2 --prefix=../../ install && 
+cd "../lib/boost/tools/build" &&
+./b2 --prefix=../../ install
 cd ../../ &&
-./bin/b2 --show-libraries && #(to test correct installation of b2 might not needed)
-./bin/b2 --with-program_options --with-graph --build-type=complete &&
+./bin/b2 --show-libraries  #(to test correct installation of b2 might not needed)
+#./bin/b2 --build-dir=build/x86 address-model=32 --stagedir=./stage/x86 --build-type=complete --layout=versioned threading=multi --with-program_options --with-graph
+./bin/b2 --build-type=complete --layout=tagged threading=multi --with-program_options --with-graph
 cd "$pwd"
 exit 0
